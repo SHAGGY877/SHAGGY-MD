1c82813e6254158ab98eef12ae6491e7c79c5f6c
SHAGGY877 <xxshaggy26@gmail.com>
2024-08-27 12:07:27 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,62 @@
+const {readEnv} = require('../lib/database')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const config = await readEnv():
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `*hello ${pushname}*👽`  
+> 👽_*DOWNLOAD COMMANDS*_👽
+
+${menu.download}
+
+> *MAIN COMMANDS* 👽
+
+${menu.main}
+
+> *GROUP COMMANDS* 👽
+
+${menu.group}
+
+> *OWNER COMMANDS* 👽
+
+${menu.owner}
+
+> *CONVERT COMMANDS* 👽
+
+${menu.convert}
+
+> *SEARCH COMMANDS* 👻
+
+${menu.search}
+
+POWERD BY Sʜᴀɢɢʏㅤヤ  
+`
+await conn.sendMessage(from,{text:image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
